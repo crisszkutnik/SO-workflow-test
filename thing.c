2b00df3aff7e4e873bb7aecf776a066375ddc91d
Guido Dipietro <dipietroguido@gmail.com>
2021-01-26 01:19:49 -0300

    prueba con branches

---
+++ thing.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()      {
+
+	printf("ben\n");
+
+	int a;
+	 int b;
+
+		int c;
+	 int d;
+			int e;
+	  int f;
+
+		int g;
+	 int h;
+	   int i;
+
+	   {}
+
+	return      (0);}
