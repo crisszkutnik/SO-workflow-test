583a767e3f79e3cc468f250e94d0b7893f18ab70
Clang Formatter <robot@example.com>
2021-01-26 03:56:53 +0000

    Committing clang-format changes

--- mess.c
+++ mess.c
@@ -1,46 +1,41 @@
 /*
-	coded BY THE SPUTNIK V GANG
+        coded BY THE SPUTNIK V GANG
 
-	esto es lo que pasa cuando te vacuna el tio alberchas
+        esto es lo que pasa cuando te vacuna el tio alberchas
 */
 
 #include <stdio.h>
 
-                      // 1984 //
-                       //    //
-                        // //
-                         //
-                    //////////////
-                    //          //
-					/* hi there */
-					//////////////
-#include              "mess.h"
+// 1984 //
+//    //
+// //
+//
+//////////////
+//          //
+/* hi there */
+//////////////
+#include "mess.h"
 //                     ^^^^^^^
 //                     ||   ||
 
-char biden[]=
+char biden[] =
 
-
-
-
-
-
-"WELCOME TO "
-""
-"CHANNEL!!";
+    "WELCOME TO "
+    ""
+    "CHANNEL!!";
 
 /*
 
-	o -( Hi, I'm a happy sticc man! Just chillin on some #defines )
-	+
-	^                                               */
-	#define ONE 1
-		#define TWO 2
-			#define PI 3
-				#define FOUR 4
-					#define FIVE 5
+        o -( Hi, I'm a happy sticc man! Just chillin on some #defines )
+        +
+        ^                                               */
+#define ONE 1
+#define TWO 2
+#define PI 3
+#define FOUR 4
+#define FIVE 5
 
-int                     square                               (int x)
+int square(int x)
 
 /*
 
@@ -54,47 +49,46 @@ int                     square                               (int x)
 
 */
 
-{                      return x*x                              ;}
+{
+  return x * x;
+}
 
-void map(int* list, unsigned szlist, int (*func)(int)) {for (int i=0; i<szlist; i++) list[i] = (*func)(list[i]);
-
-
+void map(int *list, unsigned szlist, int (*func)(int)) {
+  for (int i = 0; i < szlist; i++)
+    list[i] = (*func)(list[i]);
 }
 
 int main() {
 
-{}    {}      {{}}     {}            {}
-{}    {}    {}    {}   {}           {  }
-{{{{}}}}   {}      {}  {}          {{{}}}
-{}    {}    {}    {}   {}         {}    {}
-{}    {}      {{}}     {{{{}}}}  {}      {}
+  {} {} {{}} {} {} {} {} {} {} {} {} {{{{}}}} {} {} {} {
+      {{}}} {} {} {} {} {} {} {} {} {} {{}} {{{{}}}} {} {}
 
-// creo que enloquecí
+  // creo que enloquecí
 
-	beto "vive%s\n" en "el pueblo" argentino;
+  beto "vive%s\n" en "el pueblo" argentino;
 
+  printf("%d%d%d%d%d%d%d%d%d%d%d\n", b, e, t, o, c, a, m, p, e, o, n);
 
-	printf("%d%d%d%d%d%d%d%d%d%d%d\n", b, e, t, o, c, a, m, p, e, o, n);
+  int list_of_numbers[FIVE] = {14, 27, 49, 1, 2};
 
-	int list_of_numbers[FIVE] = {14, 27, 49, 1, 2};
+  // ebola!!!
 
-	// ebola!!!
-
-	/*
-		WOW THIS IS VERY
-		             BAD CODE
-		             // COMMENT!!
-		             /* while */ /*
+  /*
+          WOW THIS IS VERY
+                       BAD CODE
+                       // COMMENT!!
+                       /* while */ /*
 	*/
 
-	map   						(list_of_numbers,    TWO+PI-1+1,    square   )   ;
+  map(list_of_numbers, TWO + PI - 1 + 1, square);
 
-	for
+  for
 
-
-
-
-		(char pelado=0; pelado<FIVE; printf("Papa: %d\n", list_of_numbers[pelado++]));return 0;}
+      (char pelado = 0; pelado < FIVE;
+       printf("Papa: %d\n", list_of_numbers[pelado++]))
+    ;
+  return 0;
+}
 
 /* This shit actually compiles and runs properly */
 // gcc mess.c -o tasty.out
--- mess.h
+++ mess.h
@@ -1,12 +1,13 @@
 #ifndef _MESS_H
 #define _MESS_H
 
-					#include <stdio.h>
+#include <stdio.h>
 
 #define beto printf(
 #define en ,
 #define argentino )
 
-int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
+int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y,
+    z;
 
 #endif
