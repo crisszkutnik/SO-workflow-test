78509eb888f24206d14e837956cb54018571dbe6
Benjamin Paul <bpaul848@gmail.com>
2021-01-26 14:49:29 +1000

    guido said so

---
+++ hi.c
@@ -0,0 +1,15 @@
+#define main main(scanff, tao){}Q
+#define scanf printf(
+
+#																																																				pragma 																																																										once
+#include "hi.c"
+
+main(a)int a;{{{{{{
+{{{
+#define tao "hi"
+}
+
+}}}
+	return scanf(tao)
+#define tao is very cool wow omg so cool
+	);}}}}}
