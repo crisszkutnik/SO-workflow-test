0cc47e6ba8fa69975529c18d0e856679fb683374
Guido Dipietro <dipietroguido@gmail.com>
2021-01-26 00:56:17 -0300

    Buckle up.

---
+++ mess.c
@@ -0,0 +1,100 @@
+/*
+	coded BY THE SPUTNIK V GANG
+
+	esto es lo que pasa cuando te vacuna el tio alberchas
+*/
+
+#include <stdio.h>
+
+                      // 1984 //
+                       //    //
+                        // //
+                         //
+                    //////////////
+                    //          //
+					/* hi there */
+					//////////////
+#include              "mess.h"
+//                     ^^^^^^^
+//                     ||   ||
+
+char biden[]=
+
+
+
+
+
+
+"WELCOME TO "
+""
+"CHANNEL!!";
+
+/*
+
+	o -( Hi, I'm a happy sticc man! Just chillin on some #defines )
+	+
+	^                                               */
+	#define ONE 1
+		#define TWO 2
+			#define PI 3
+				#define FOUR 4
+					#define FIVE 5
+
+int                     square                               (int x)
+
+/*
+
+             -----
+             |   |
+             |   |
+             -----
+
+             there's your goddamn square
+
+
+*/
+
+{                      return x*x                              ;}
+
+void map(int* list, unsigned szlist, int (*func)(int)) {for (int i=0; i<szlist; i++) list[i] = (*func)(list[i]);
+
+
+}
+
+int main() {
+
+{}    {}      {{}}     {}            {}
+{}    {}    {}    {}   {}           {  }
+{{{{}}}}   {}      {}  {}          {{{}}}
+{}    {}    {}    {}   {}         {}    {}
+{}    {}      {{}}     {{{{}}}}  {}      {}
+
+// creo que enloquecí
+
+	beto "vive%s\n" en "el pueblo" argentino;
+
+
+	printf("%d%d%d%d%d%d%d%d%d%d%d\n", b, e, t, o, c, a, m, p, e, o, n);
+
+	int list_of_numbers[FIVE] = {14, 27, 49, 1, 2};
+
+	// ebola!!!
+
+	/*
+		WOW THIS IS VERY
+		             BAD CODE
+		             // COMMENT!!
+		             /* while */ /*
+	*/
+
+	map   						(list_of_numbers,    TWO+PI-1+1,    square   )   ;
+
+	for
+
+
+
+
+		(char pelado=0; pelado<FIVE; printf("Papa: %d\n", list_of_numbers[pelado++]));return 0;}
+
+/* This shit actually compiles and runs properly */
+// gcc mess.c -o tasty.out
