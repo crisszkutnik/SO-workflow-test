a62f54439ab2d791e434c1b534c6dbaa8f071ace
Cristobal Szkutnik <crisszkutnik@gmail.com>
2021-01-27 02:07:46 -0300

    Fixed to script

--- run-test.sh
+++ run-test.sh
@@ -26,7 +26,21 @@ tests=$(find ./test/compile/ -type f -name "*.out")
 
 valgrind_args="--leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --error-exitcode=1"
 counter=1
-max=${#valgrind_args[@]}
+
+max=0
+
+
+# This is not a good way to do this but it wokrks
+for e in ${tests[@]} 
+do
+	max=$(($max + 1))
+done
+
+# No tests to perform. Exit now
+if ((max == 0));
+then
+	exit 0
+fi
 
 RED="\033[0;31m"
 NC="\033[0m"
@@ -41,14 +55,20 @@ EXIT_STATUS=0
 declare -a MEM_LEAK
 declare -a TEST_ERROR
 
+#
+#
+# Start performing tests
+#
+#
+
 printf "\n\n===== Perform tests =====\n\n"
 
-for e in $tests
+for e in ${tests[@]}
 do
 	# first, run the test
 
 	TEST_COUNTER="($counter/$max)"
-	./$e
+	./$e *> /dev/null &
 	EXIT_CODE_1=$?
 
 	if (($EXIT_CODE_1 == 0));
@@ -65,20 +85,23 @@ do
 		then
 			MEM_LEAK+=($e)
 			EXIT_STATUS=2
-			echo -e "[${RED}X${NC}]  ${e} - ${RED}Valgrind detected memory leak${NC} ${TEST_COUNTER}"
+			printf "[${RED}X${NC}]  ${e} - ${RED}Valgrind detected memory leak${NC} ${TEST_COUNTER}\n\n"
 		else
-			echo -e "[${GREEN}Success${NC}] ${e} - ${GREEN}Valgrind detected no memory leaks${NC} ${TEST_COUNTER}"
+			printf "[${GREEN}Success${NC}] ${e} - ${GREEN}Valgrind detected no memory leaks${NC} ${TEST_COUNTER}\n\n"
 		fi
 	else
 		EXIT_STATUS=1
 		TEST_ERROR+=($e)
 		echo -e "[${RED}X${NC}] ${e} - ${RED}Test failed${NC} ${TEST_COUNTER}"
-		echo "Valgrind test for $e were not performed"
-		printf "\n"
+		printf "Valgrind test for $e were not performed\n\n"
 	fi
 
 	counter=$(($counter + 1))
 done
+
+#
+# If on GitHub, make request to GitGariol
+#
 
 if [ ! -z "$1" ] && [ "$1" == "github" ];
 then
@@ -88,7 +111,7 @@ then
 	then
 		URL_DIR+="&1"
 
-		for e in $TEST_ERROR
+		for e in ${TEST_ERROR[@]}
 		do
 			PARSED_STR=${e##*/}
 			URL_DIR+="*${PARSED_STR[${#PARSED_STR[@]} - 1]}"
@@ -99,7 +122,7 @@ then
 	then
 		URL_DIR+="&2"
 
-		for e in $MEM_LEAK
+		for e in ${MEM_LEAK[@]}
 		do
 			PARSED_STR=${e##*/}
 			URL_DIR+="*${PARSED_STR[${#PARSED_STR[@]} - 1]}"
